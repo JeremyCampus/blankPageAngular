a1d94b1d0e35b7fd5755becfe4a9bc7e8dbfee06
LAURENS <jeremy.laurens@capgemini.com>
2018-06-28 11:59:54 +0200

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthComponent } from './auth/auth.component';
+import { SecondViewComponent } from './second-view/second-view.component';
+import {RouterModule, Routes} from '@angular/router';
+import { FirstViewComponent } from './first-view/first-view.component';
+import { AuthService } from './services/auth.service';
+
+const appRoutes: Routes=[
+  {path: 'FirstView', component: FirstViewComponent },
+  {path: 'SecondView', component: SecondViewComponent },
+  {path: 'Auth', component: AuthComponent },
+  {path: '', component: SecondViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AuthComponent,
+    FirstViewComponent,
+    SecondViewComponent,
+    FirstViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
